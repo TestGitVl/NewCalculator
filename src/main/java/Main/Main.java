d77947f2d92937f11fbcda882e60ec752f2236d5
VladislavPaddeus <paddeusvladislav@gmail.com>
2024-11-10 09:49:29 +0400

    База калькулятора

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,8 +1,14 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
         System.out.println("Калькулятор запущен\n");
+
+        int x = in.nextInt();
+        int y = in.nextInt();
+        String operation = in.next();
     }
 }
